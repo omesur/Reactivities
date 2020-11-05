ef8da1a86a5e5500a6c4aa4abb3e35988e6b55d2
omesur <omesur@gmail.com>
2020-11-05 17:33:20 -0500

    Delete practice

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { cars } from './demo';
-import CarItem from './CarItem';
 
-const App: React.FC = () => {
+function App() {
   return (
     <div className="App">
-      <ul>
-        {cars.map((car) => (
-          <CarItem />
-        ))}
-      </ul>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
