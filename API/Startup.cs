3bb56c826bed4255ba4cc40237257372d7325b70
omesur <omesur@gmail.com>
2020-11-06 15:05:44 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,11 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             }
             );
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -49,6 +54,8 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
