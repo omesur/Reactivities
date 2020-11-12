6b73f7749f755b137206c613bbf6c3d8b6eb6b0a
omesur <omesur@gmail.com>
2020-11-12 14:34:10 -0500

    End of section 4

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using Domain;
+﻿using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options)
         {            
         }
 
         public DbSet<Value> Values {get;set;}
+
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
